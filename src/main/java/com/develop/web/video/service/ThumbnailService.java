4cb220c831d565afcd6c67ff69c80666180abafa
solarpark7346 <solarpark7346@gmail.com>
2023-09-01 05:51:31 +0900

    add: 썸네일 생성 서비스 추가

---
+++ src/main/java/com/develop/web/video/service/ThumbnailService.java
@@ -0,0 +1,36 @@
+package com.develop.web.video.service;
+
+import com.develop.web.utils.VideoFileUtils;
+import com.develop.web.video.dto.FolderDto;
+import com.develop.web.video.dto.Metadata;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ThumbnailService {
+    private final VideoFileUtils videoFileUtils;
+    private final FolderDto folder;
+
+    public void markThumbnail(Metadata convertResultMetadata) {
+        String inputPath = convertResultMetadata.file_path;
+        String outputPath = folder.thumbnailDir;
+
+        FFmpegBuilder builder = new FFmpegBuilder()
+            .overrideOutputFiles(true)
+            .setInput(inputPath)
+            .addExtraArgs("-ss", "00:00:03")
+            .addOutput(outputPath + "/" + convertResultMetadata.clip_uuid + ".jpg")
+            .setFrames(1)
+            .done();
+
+        FFmpegExecutor executor = new FFmpegExecutor(videoFileUtils.ffmpeg, videoFileUtils.ffprobe);
+        executor.createJob(builder).run();
+
+        log.info("[!] " + convertResultMetadata.filename + " 썸네일을 생성하였습니다. ");
+    }
+}
